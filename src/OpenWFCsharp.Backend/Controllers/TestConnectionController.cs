04873259c5d9f907bc81fd26dc8ce98843602e0e
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-15 15:37:39 +0100

    :sparkles: Set server port and more accurate test page

--- src/OpenWFCsharp.Backend/Controllers/TestConnectionController.cs
+++ src/OpenWFCsharp.Backend/Controllers/TestConnectionController.cs
@@ -1,6 +1,7 @@
 ﻿namespace OpenWFCsharp.Backend.Controllers;
 
 using System.Diagnostics;
+using System.Net.Mime;
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 public class TestConnectionController : ControllerBase
 {
     private const string DevVersion = "0.0.0-dev";
-    private static readonly string AppVersion = GetAppVersion();
+    private static readonly string HtmlTestPage = GetTestPage();
 
     /// <summary>
     /// HTTP GET to check Internet connection.
@@ -23,7 +24,28 @@ public class TestConnectionController : ControllerBase
     {
         Response.Headers.Append("X-Organization", "PleOps");
         Response.Headers.Append("Server", "OpenWFCSharp");
-        return Ok($"Hello from OpenWFCsharp {AppVersion} by pleonex");
+        return Content(HtmlTestPage, MediaTypeNames.Text.Html);
+    }
+
+    private static string GetTestPage()
+    {
+        // This was from the original test page in 2023
+        return @"
+<!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.0 Transitional//EN"" ""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"">
+<html>
+    
+<head>
+		<title>HTML Page</title>
+</head>
+
+<body bgcolor=""#FFFFFF"">
+<!-- This is test.html page -->
+Hello from OpenWFCsharp {AppVersion} by pleonex
+</body>
+    
+</html>
+"
+        .Replace("{AppVersion}", GetAppVersion());
     }
 
     private static string GetAppVersion()
