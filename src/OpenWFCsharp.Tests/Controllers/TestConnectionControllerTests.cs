ca8abaa1dcd646bbe360db32b695aa38fc6e8e7f
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-16 11:27:51 +0100

    :umbrella: Test connection tests

---
+++ src/OpenWFCsharp.Tests/Controllers/TestConnectionControllerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace OpenWFCsharp.Tests.Controllers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenWFCsharp.Backend.Controllers;
+
+[TestFixture]
+public class TestConnectionControllerTests
+{
+    private TestConnectionController controller;
+
+    [SetUp]
+    public void TestSetUp()
+    {
+        controller = new TestConnectionController {
+            ControllerContext = new ControllerContext() {
+                HttpContext = new DefaultHttpContext(),
+            },
+        };
+    }
+
+    [Test]
+    public void PingReturns200()
+    {
+        var result = controller.Ping();
+
+        Assert.That(result, Is.InstanceOf<ContentResult>());
+    }
+
+    [Test]
+    public void PingSetsAdditionalHeaders()
+    {
+        var result = controller.Ping();
+        var contentResult = result as ContentResult;
+
+        Assert.That(result, Is.InstanceOf<ContentResult>());
+        Assert.That(contentResult!.ContentType, Is.EqualTo("text/html"));
+        Assert.That(controller.Response.Headers, Contains.Key("X-Organization"));
+        Assert.That(controller.Response.Headers, Contains.Key("Server"));
+    }
+}
